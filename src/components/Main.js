03d7e142cc0496074f56fadac0190a42b48dba45
Kadir Koç <kadiirkkoc@gmail.com>
2023-04-14 04:45:41 +0200

    icons can not executed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,35 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope  } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Main() {
     return (
         <>
             <div className="main-container">
-                <FontAwesomeIcon icon={light("envelope")} />
+                <div className="buttons">
+                    <button className="btn btn-1">
+                        <a href="kadiirkkoc@gmail.com">Email</a>
+                    </button>
+
+                    <button className="btn btn-2">
+                        <a href="https://www.linkedin.com/in/kadir-koc-63a0a1257/">Linkedin</a>
+                    </button>
+                </div>
+
+                <div className="explain">
+                    <span className="about ">
+                        <h1>About</h1>
+                        <p>
+                            I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+                        </p>
+                    </span>
+                    <span className="interests">
+                        <h1>Interests</h1>
+                        <p>
+                            I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
+                        </p>
+                    </span>
+                </div>
             </div>
 
 
